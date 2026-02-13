1bd0070c4482a634692ade2eb8df3bab0ced58e2
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-13 14:32:21 -0600

    try this

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let school = {
   'average class size': 12, 
   'mascot': 'tiger',
 };
-let description = `${school.name} has ${school['average class size']} students and rep the ${school['mascot']}.`
+let description = `${name} has an average class size of ${average class size} and their mascot is a ${mascot}`
 
 document.getElementById("result").innerHTML = description;
+
 
 
 
