8c2ab09d6733df9049ae2faa465394c9b6a15f41
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-13 14:25:15 -0600

    check this one

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let school = {(name, averageClassSize, mascot)};
+let school = {
+  'name': 'Hannah', 
+  'average class size': 12, 
+  'mascot': 'tiger',
+};
+let description = `${school.name} has ${school['average class size']} students and rep the ${school['mascot']}.`
 
-document.getElementById("result").innerHTML = ${name} ${averageClassSize} ${mascot} ;
+document.getElementById("result").innerHTML = description;
+
 
 
 
