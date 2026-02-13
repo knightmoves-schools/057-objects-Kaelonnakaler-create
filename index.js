29eb9001f1d0ac9dff181c92558174b333ca7923
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-13 14:39:00 -0600

    now try

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let school = {
   'average class size': 12, 
   'mascot': 'tiger',
 };
-let description = `${school.name} has an average class size of ${school.'average class size'} and their mascot is a ${school.'mascot'}`
+let description = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school['mascot']}`
 
 document.getElementById("result").innerHTML = description;
+
 
 
 
